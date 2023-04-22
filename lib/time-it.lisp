67e6e378313d54d76c25e41a5e93cd9e00fcf27d
Max Mickey <max.mickey1991@gmail.com>
2023-04-22 17:44:10 -0500

    time-it library now reports a float

--- lib/time-it.lisp
+++ lib/time-it.lisp
@@ -5,7 +5,7 @@
   (let ((start-sym (gensym "TIME-START")))
     `(let ((,start-sym (get-clock-ticks)))
        ,expr
-       (let ((,time-var (/ (- (get-clock-ticks) ,start-sym)
+       (let ((,time-var (fdiv (- (get-clock-ticks) ,start-sym)
                            1000)))
          ,@body))))
 
